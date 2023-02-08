740621610f6f95341a7d2e402c82d8598c1f1624
scarfniyato <123631696+scarfniyato@users.noreply.github.com>
2023-02-08 22:39:16 +0800

    Add files via upload

---
+++ username/test1/test2/test3/javascript/questions.js
@@ -0,0 +1,19 @@
+//TEST 3 QUES
+let questions = [
+    {
+    numb: 1,
+    question: "<br>" + "&emsp;&emsp;&emsp;If a scarcity of commodities develops, then the prices rise. If there is a change of government, then" + 
+                "<br>" + "&emsp;&emsp;&emsp;fiscal controls will not be continued. If the threat of inflation persists, then fiscal controls will be" +
+                "<br>" + "&emsp;&emsp;&emsp;continued. If there is overproduction, then prices do not rise. Either there is overproduction or there" +
+                "<br>" + "&emsp;&emsp;&emsp;is a change of government. Therefore, either a scarcity of commodities does not develop or there" + 
+                "<br>" + "&emsp;&emsp;&emsp;is a change of government." +
+                "<br>" + "&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span>S:</span>&emsp;Scarcity develops&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;<span>F:</span>&emsp;Fiscal controls continued" + 
+                "<br>" + "&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span>P:</span>&emsp;Prices rise&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;<span>I:</span>&emsp;Inflation persists" + 
+                "<br>" + "&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span>G:</span>&emsp;Government changes&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span>O:</span>&emsp;Overproduction occurs",
+    answer: "1.)    S → P2.)   G → ¬ F3.)   I → F4.)   O → ¬ P5.)   (O ^ ¬ G) v (¬ O ^ G)  /    (¬ S ^ ¬ G) v (S ^ G)",
+    options: [
+        "<pre>1.)    S → P<br>2.)   G → ¬ F<br>3.)   I → F<br>4.)   O → ¬ P<br>5.)   (O ^ ¬ G) v (¬ O ^ G)<br>  /    (¬ S ^ ¬ G) v (S ^ G)</pre>",
+        "<pre>1.)    S v F<br>2.)   O → F<br>3.)   P → F<br>4.)   O ^ P<br>5.)   (G ^ ¬ 0) v (¬ G ^ O)<br>  /    (¬ S v ¬ G) ^ (S v G)</pre>",
+        ]
+    },
+];
