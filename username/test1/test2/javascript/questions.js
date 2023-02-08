7937eb954318cb83f5fd7c6b841e7595a2e39dbe
scarfniyato <123631696+scarfniyato@users.noreply.github.com>
2023-02-08 22:38:34 +0800

    Add files via upload

---
+++ username/test1/test2/javascript/questions.js
@@ -0,0 +1,48 @@
+//TEST 2 QUES
+let questions = [
+  {
+  numb: 1,
+  question: "<br>" + "&emsp;&emsp;" + "1." + "&emsp;&emsp;" + "¬ J v M" + "<br>" + "&emsp;&emsp;" + "2." + "&emsp;&emsp;" + "M → B" + "<br>" + "&emsp;&emsp;" + "3." + "&emsp;&emsp;" + "__________" + "<br>" + "&emsp;&emsp;" + "4." + "&emsp;&emsp;" + "J → B" + "&emsp;" + "______",
+  answer: "3.) J → M      1 Impl                4.) 2, 3  HS",
+  options: [
+  "<pre>3.) J → M      1 Impl                4.) 2, 3  HS</pre>",
+  "<pre>3.) J v M      1 Exp                   4.) 1, 3  CD</pre>",
+  ]
+},
+  {
+  numb: 2,
+  question: "<br>" + "&emsp;&emsp;" + "1." + "&emsp;&emsp;" + "(G → K ) ^ (T → K )" + "<br>" + "&emsp;&emsp;" + "2." + "&emsp;&emsp;" + "G v T" + "<br>" + "&emsp;&emsp;" + "3." + "&emsp;&emsp;" + "_______________" + "<br>" + "&emsp;&emsp;" + "4." + "&emsp;&emsp;" + "K" + "&emsp;&emsp;" + "________",
+  answer: "3.) K v K    1, 2 CD                 4.) 3  Taut",
+  options: [
+    "<pre>3.) K v K    1, 2 CD                 4.) 3  Taut</pre>",
+    "<pre>3.) K → K    1, 2 CD               4.) 1  Taut</pre>",
+  ]
+},
+  {
+  numb: 3,
+  question: "<br>" + "&emsp;&emsp;" + "1." + "&emsp;&emsp;" + "H → (¬ C v R)" + "<br>" + "&emsp;&emsp;" + "2." + "&emsp;&emsp;" + "___________" + "<br>" + "&emsp;&emsp;" + "3." + "&emsp;&emsp;" + "(H ^ C ) → R" + "&emsp;" + "____",
+  answer: "2.) H → (C → R)    1 Impl            3.) 2  Exp",
+  options: [
+    "<pre>2.) H → (C → R)    1 Dist             3.) 3  Exp</pre>",
+    "<pre>2.) H → (C → R)    1 Impl            3.) 2  Exp</pre>",
+    ]
+},
+  {
+  numb: 4,
+  question: "<br>" + "&emsp;&emsp;" + "1." + "&emsp;&emsp;" + "H → ¬ H" + "<br>" + "&emsp;&emsp;" + "2." + "&emsp;&emsp;" + "___________" + "<br>" + "&emsp;&emsp;" + "3." + "&emsp;&emsp;" + "¬ H" + "&emsp;&emsp;" + "_______",
+  answer: "2.) ¬ H v ¬ H    1 Impl            3.) 2  Taut",
+  options: [
+    "<pre>2.) ¬ H ^ ¬ H    1 DM              3.) 2  DN</pre>",
+    "<pre>2.) ¬ H v ¬ H    1 Impl            3.) 2  Taut</pre>",
+  ]
+},
+  {
+  numb: 5,
+  question: "<br>" + "&emsp;&emsp;" + "1." + "&emsp;&emsp;" + "(N → A) ^ (¬ N → ¬ A)" + "<br>" + "&emsp;&emsp;" + "2." + "&emsp;&emsp;" + "_________________" + "<br>" + "&emsp;&emsp;" + "3." + "&emsp;&emsp;" + "N ↔ A" + "&emsp;&emsp;" + "_______",
+  answer: "2.) (N → A) ^ (A → N)     1, Trans         3.) 2  Equiv",
+  options: [
+    "<pre>2.) (N → A) ^ (A → N)     1, Trans         3.) 2  Equiv<pre>",
+    "<pre>2.) (N → A) ^ (A → N)     1, Com           3.) 2 Equiv<pre>",
+  ]
+},
+];
