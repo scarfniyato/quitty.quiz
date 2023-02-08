5e3adddfbf0944f8ee197a1fb0f1c4116b613618
scarfniyato <123631696+scarfniyato@users.noreply.github.com>
2023-02-08 22:37:30 +0800

    Add files via upload

---
+++ username/test1/javascript/questions.js
@@ -0,0 +1,92 @@
+let questions = [
+    {
+    numb: 1,
+    question: "<br>" + "&emsp;&emsp;" + "1." + "&emsp;&emsp;" + "K → ( H ^ T )" + "<br>" + "&emsp;&emsp;" + "2." + "&emsp;&emsp;" + "( H ^ T) → G" + "<br>" + "&emsp;&emsp;" + "3." + "&emsp;&emsp;" + " / K → G",
+    answer: "Hypothetical Syllogism",
+    options: [
+      "Hypothetical Syllogism",
+      "Constructive Dilemma",
+    ]
+  },
+    {
+    numb: 2,
+    question: "<br>" + "&emsp;&emsp;" + "1." + "&emsp;&emsp;" + "( A ^ D ) v ( A ^ P )" + "<br>" + "&emsp;&emsp;" + "2." + "&emsp;&emsp;" + "/ A ^ ( D v P )",
+    answer: "Distribution",
+    options: [
+      "Addition",
+      "Distribution",
+    ]
+  },
+    {
+      numb: 3,
+      question: "<br>" + "&emsp;&emsp;" + "1." + "&emsp;&emsp;" + "¬ F → ¬ R" + "<br>" + "&emsp;&emsp;" + "2." + "&emsp;&emsp;" + "/ R → F",
+      answer: "Transposition",
+      options: [
+        "Tautology",
+        "Transposition",
+      ]
+  },
+    {
+    numb: 4,
+    question: "<br>" + "&emsp;&emsp;" + "1." + "&emsp;&emsp;" + "Q → ( O → H )" + "<br>" + "&emsp;&emsp;" + "2." + "&emsp;&emsp;" + "/ ( Q ^ O ) → H",
+    answer: "Exportation",
+    options: [
+      "Exportation",
+      "Modus Tollens",
+    ]
+  },
+    {
+    numb: 5,
+    question: "<br>" + "&emsp;&emsp;" + "1." + "&emsp;&emsp;" + "( L → W) ^ (W → L)" + "<br>" + "&emsp;&emsp;" + "2." + "&emsp;&emsp;" + "/ L ↔ W",
+    answer: "Material Equivalence",
+    options: [
+      "De Morgan's Rule",
+      "Material Equivalence",
+    ]
+  },
+    {
+    numb: 6,
+    question: "<br>" + "&emsp;&emsp;" + "1." + "&emsp;&emsp;" + "S v ( E v L)" + "<br>" + "&emsp;&emsp;" + "2." + "&emsp;&emsp;" + "¬ L" + "<br>" + "&emsp;&emsp;" + "3." + "&emsp;&emsp;" + "( S v E ) v L" + "<br>" + "&emsp;&emsp;" + "4." + "&emsp;&emsp;" + "/ L v ( S v E )",
+    answer: "Commutativity",
+    options: [
+      "Associativity",
+      "Commutativity",
+    ]
+  },
+    {
+    numb: 7,
+    question: "<br>" + "&emsp;&emsp;" + "1." + "&emsp;&emsp;" + "A → O" + "<br>" + "&emsp;&emsp;" + "2." + "&emsp;&emsp;" + "¬ A v O" + "<br>" + "&emsp;&emsp;" + "3." + "&emsp;&emsp;" + "( ¬A v O ) v G" + "<br>" + "&emsp;&emsp;" + "4." + "&emsp;&emsp;" + "¬ A v ( O v G )" + "<br>" + "&emsp;&emsp;" + "5." + "&emsp;&emsp;" + "/ A → ( O v G )",
+    answer: "Material Implication",
+    options: [
+      "Material Implication",
+      "Addition",
+    ] 
+  },
+    {
+    numb: 8,
+    question: "<br>" + "&emsp;&emsp;" + "1." + "&emsp;&emsp;" + "¬ C" + "<br>" + "&emsp;&emsp;" + "2." + "&emsp;&emsp;" + "¬ J" + "<br>" + "&emsp;&emsp;" + "3." + "&emsp;&emsp;" + "[ C → ( X v J ) → J ]" + "<br>" + "&emsp;&emsp;" + "4." + "&emsp;&emsp;" + "/ ¬C → ( X v J )",
+    answer: "Modus Tollens",
+    options: [
+      "Constructive Dilemma",
+      "Modus Tollens",
+    ]
+  },
+    {
+    numb: 9,
+    question: "<br>" + "&emsp;&emsp;" + "1." + "&emsp;&emsp;" + "( ¬P → Q ) ^ ( ¬R → S )" + "<br>" + "&emsp;&emsp;" + "2." + "&emsp;&emsp;" + "¬ ( P ^ R )" + "<br>" + "&emsp;&emsp;" + "3." + "&emsp;&emsp;" + "¬ P v ¬ R" + "<br>" + "&emsp;&emsp;" + "4." + "&emsp;&emsp;" + "/ Q v S",
+    answer: "Constructive Dilemma",
+    options: [
+      "Constructive Dilemma",
+      "Modus Tollens",
+    ]
+  },
+    {
+    numb: 10,
+    question: "<br>" + "&emsp;&emsp;" + "1." + "&emsp;&emsp;" + "¬ K ^ M" + "<br>" + "&emsp;&emsp;" + "2." + "&emsp;&emsp;" + "¬ K v ¬ M",
+    answer: "De Morgan's Rule",
+    options: [
+      "Disjunctive Syllogism",
+      "De Morgan's Rule",
+    ]
+  },
+];
